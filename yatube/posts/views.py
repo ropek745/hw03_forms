efdb905c87aa7b0eb7242833a4dacb28354af570
Роман Пекарев <hawkdanilow@yandex.ru>
2022-03-22 13:59:53 +0300

    Третий

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,61 +1,57 @@
-from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm
-
-from .models import Post, Group, User
-
+from .models import Group, Post, User
 from .paginator import paginator_page
 
 
 def index(request):
-    context = {
+    return render(request, 'posts/index.html', {
         'page_obj': paginator_page(request, Post.objects.all()),
-    }
-    return render(request, 'posts/index.html', context)
+    })
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    context = {
+    return render(request, 'posts/group_list.html', {
         'group': group,
         'page_obj': paginator_page(request, group.posts.all())
-    }
-    return render(request, 'posts/group_list.html', context)
+    })
 
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
-    context = {
+    return render(request, 'posts/profile.html', {
         'author': author,
         'page_obj': paginator_page(request, author.posts.all())
-    }
-    return render(request, 'posts/profile.html', context)
+    })
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    # Здесь код запроса к модели и создание словаря контекста
-    context = {
+    return render(request, 'posts/post_detail.html', {
         'post': post,
-    }
-    return render(request, 'posts/post_detail.html', context)
+    })
 
 
 @login_required
 def post_create(request):
     form = PostForm(request.POST or None)
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.save()
-        return redirect('posts:profile', post.author)
-    return render(request, 'posts/create_post.html', {'form': form})
+    if not form.is_valid():
+        return render(
+            request,
+            'posts/create_post.html',
+            {'form': form}
+        )
+    post = form.save(commit=False)
+    post.author = request.user
+    post.save()
+    return redirect('posts:profile', post.author)
 
 
 @login_required
 def post_edit(request, post_id):
-    is_edit = True
     post = get_object_or_404(Post, id=post_id)
     if request.user != post.author:
         return redirect('posts:post_detail', post_id)
@@ -63,5 +59,6 @@ def post_edit(request, post_id):
     if form.is_valid():
         form.save()
         return redirect('posts:post_detail', post_id)
-    return render(request, 'posts/create_post.html',
-                  context={'form': form, 'is_edit': is_edit, 'post': post})
+    return render(request, 'posts/create_post.html', {
+        'form': form, 'is_edit': True, 'post': post
+        })
